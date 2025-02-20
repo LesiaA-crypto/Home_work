7522d57c2b5360d687bfdcc615293a133b4f98de
Alesia <alesya.lysyuk@skz.by>
2025-02-20 22:10:19 +0300

    add another files in L8

---
+++ L8/Units_coverage_example/test_main.py
@@ -0,0 +1,6 @@
+import main
+
+
+def test_no_letters():
+    assert main.howmanyletters("") == "no data"
+    
---
+++ L8/Units_coverage_example/test_no_leters.py
@@ -0,0 +1,14 @@
+import main
+
+
+def test_no_letters():
+    assert main.howmanyletters("") == "no data"
+
+
+def test_less_than_3_letters():
+    assert main.howmanyletters("NO") == "less that three letters!?"
+
+
+def test_more_that_or_3_letters():
+    assert main.howmanyletters("lol :)") == ["lol", ":)"]
+    
